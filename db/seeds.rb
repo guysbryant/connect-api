467d1bd0fa4fb6b4196ea308f0355c414a3f0fbd
guysbryant <guy.s.bryant@gmail.com>
2021-06-05 20:31:08 -0400

    add circle seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+circle1 = Circle.find_or_create_by(name: 'test1')
+circle2 = Circle.find_or_create_by(name: 'test2')
