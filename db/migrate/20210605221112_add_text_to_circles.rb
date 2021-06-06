c8557774881e5c168d449816876d2dd3146165d4
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 09:46:25 -0400

    Remove text from circles...adding that was a mistake

--- db/migrate/20210605221112_add_text_to_circles.rb
+++
@@ -1,5 +0,0 @@
-class AddTextToCircles < ActiveRecord::Migration[6.1]
-  def change
-    add_column :circles, :text, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_221112) do
+ActiveRecord::Schema.define(version: 2021_06_04_004214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2021_06_05_221112) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "text"
   end
 
   create_table "posts", force: :cascade do |t|
